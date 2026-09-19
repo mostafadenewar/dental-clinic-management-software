fc9c12c45d1fe1a66209f922a381f001ee62900b
mostafa <mostafa.sami123@yahoo.com>
2026-09-19 06:03:10 +0300

    refactor: restructure application layout and remove unused assets
    - Updated index.html title for clarity
    - Removed unused SVG files
    - Cleaned up App.css by removing unnecessary styles
    - Implemented Header, SideBar, and MainDashboard components
    - Added basic styling for Header and SideBar

---
+++ src/left_sidebar/sideBar.tsx
@@ -0,0 +1,12 @@
+
+import './sideBar.css'
+
+const SideBar = () => {
+    return (
+        <div className="sidebar">
+            <img src="src\assets\react.svg" alt="Logo" />
+        </div>
+    )
+}
+
+export default SideBar
